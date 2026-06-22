5686b5219c66615ebd5231063742e54cfd9e2aed
Chen Bo Zhong <d1321319@o365.fcu.edu.tw>
2026-06-22 23:37:35 +0800

    modify

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 👇 新增這行，請確保這裡的名稱與你在 GitHub 建立的 Repository 名稱完全一致
+  base: '/campus-food-finder/', 
 })
